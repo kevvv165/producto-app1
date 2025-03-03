5f37936d581930ad8a4a14f9cbd1328277366545
PanchoHdez <hdezfran0204@gmail.com>
2025-03-03 14:39:40 -0600

    Create Add.js

---
+++ Add.js
@@ -0,0 +1,10 @@
+ import React from &#39;react&#39;;
+import ProductList from &#39;./ProductList&#39;;
+const App = () =&gt; {
+return (
+&lt;div className=&quot;App&quot;&gt;
+&lt;ProductList /&gt;
+&lt;/div&gt;
+);
+}
+export default App;
